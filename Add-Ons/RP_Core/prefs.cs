3bc59603a4e2fc83262a548fbc00524962ab0540
Gordo12699 <Gordo12699@gmail.com>
2015-03-09 13:54:16 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Core/prefs.cs
@@ -0,0 +1,31 @@
+// --------------------------
+// RP Core :: Standard preferences
+// --------------------------
+
+// Server
+$RP::pref::server::debug = 1;
+$RP::pref::server::visibleDistance = 50; // The distance to hear you and see you
+$RP::pref::server::localChat = 1; // Local chat is enabled
+$RP::pref::server::stats = 1; // If stats is enabled
+$RP::pref::server::capsFilter = 1;
+$RP::pref::server::report = 0;
+$RP::pref::server::suggest = 0;
+$RP::pref::server::rankAdvanceCommands = 0; // What rank that is necessary to use advanced commands
+
+// User
+
+$RP::pref::user::updateInterval = 1; // The updateinterval for bottom print
+$RP::pref::user::amountTools = 7;
+$RP::pref::user::lastActionTimer = 1; // The limit before next time you can click something
+$RP::pref::user::stopActivate = 20; // Seconds before the activation on clickable bricks vanishes
+$RP::pref::user::displayExp = 1; // Choose to display experience
+$RP::pref::user::displayBottomPrint = 1; // Choose to display bottomprint at all
+$RP::pref::user::playerScaling = 0; // Choose to lock scaling of playermodel
+$RP::pref::user::playerScale = "1 1 1"; // Player skaling
+$RP::pref::user::playerModel = "RP_Player"; // An empty string will allow you to change player as you'd like
+
+// Misc
+
+$RP::pref::canJet = 0;
+$RP::pref::minJetEnergy = 0;
+$RP::pref::jetEnergyDrain = 0;
