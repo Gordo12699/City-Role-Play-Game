6ed3356bc6b427db715df18580debea1dd99fb3d
Gordo12699 <Gordo12699@gmail.com>
2015-03-08 19:18:01 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Time/prefs.cs
@@ -0,0 +1,19 @@
+// --------------------------
+// RP Time :: Standard preferences
+// --------------------------
+
+// Game
+
+$RP::pref::game::tick = 1;
+$RP::pref::game::am_pm = 1;
+
+// User
+
+$RP::pref::user::tickExp = 1;
+
+// Misc
+
+$RP::pref::timeHour = 1;
+$RP::pref::timeDay = 1;
+$RP::pref::timeMonth = 1;
+$RP::pref::timeYear = 0;
