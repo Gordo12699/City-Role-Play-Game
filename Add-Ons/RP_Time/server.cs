e5df7796ed6fd4d6f354ab59e0f6774fbd80adfe
Gordo12699 <Gordo12699@gmail.com>
2015-03-09 12:23:22 -0700

    Update server.cs
    Now executes daycycles.cs

--- Add-Ons/RP_Time/server.cs
+++ Add-Ons/RP_Time/server.cs
@@ -19,6 +19,7 @@ else if (isFunction(RPModExist) && isFunction(CreateRPDB))
 	RPRegMod("Time");
 	
 	// Run scripts
+	exec("./daycycles.cs");
 	exec("./system.cs");
 	exec("./scriptObject.cs");
 	exec("./game.cs");
