c0a142778c534615ee694a3ab578cd5a60afdb95
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 16:55:47 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Jobs/prefs.cs
@@ -0,0 +1,14 @@
+// --------------------------
+// RP Jobs :: Standard preferences
+// --------------------------
+
+// Server
+
+$RP::pref::server::autoDeclineTime = 15;
+
+// User
+
+$RP::pref::user::promoteExp = 50;
+
+$RP::pref::server::ShopLicenseCost = 10000;
+$RP::pref::server::PayCheck::Max = 2500;
