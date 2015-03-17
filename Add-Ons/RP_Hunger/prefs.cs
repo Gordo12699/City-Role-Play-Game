5098586db775fb252ddf92e764cbf57e731bbd95
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 16:50:06 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Hunger/prefs.cs
@@ -0,0 +1,48 @@
+// ============================================================
+// Section 8 : Food
+// ============================================================
+$CRPG::portion[1] = "Small";
+$CRPG::portion[2] = "Medium";
+$CRPG::portion[3] = "Large";
+$CRPG::portion[4] = "Extra-Large";
+$CRPG::portion[5] = "Super-Sized";
+$CRPG::portion[6] = "Americanized";
+
+// ============================================================
+// Section 10 : Hunger Colors and States
+// ============================================================
+$CRPG::food::stateCount = 0;
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "FF0000";
+$CRPG::food::state[$CRPG::food::stateCount] = "10<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "FD2000";
+$CRPG::food::state[$CRPG::food::stateCount] = "20<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "FF5900";
+$CRPG::food::state[$CRPG::food::stateCount] = "30<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "FD7E00";
+$CRPG::food::state[$CRPG::food::stateCount] = "40<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "FD7E00";
+$CRPG::food::state[$CRPG::food::stateCount] = "50<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "F7FD00";
+$CRPG::food::state[$CRPG::food::stateCount] = "60<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "C6FF00";
+$CRPG::food::state[$CRPG::food::stateCount] = "70<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "7EFD00";
+$CRPG::food::state[$CRPG::food::stateCount] = "80<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "73FF00";
+$CRPG::food::state[$CRPG::food::stateCount] = "90<color:ffffff>%";
+
+$CRPG::food::color[$CRPG::food::stateCount++] = "00FF00";
+$CRPG::food::state[$CRPG::food::stateCount] = "100<color:ffffff>%";
+
+$testingstatus = 0;
+
+$growticks = 0;
