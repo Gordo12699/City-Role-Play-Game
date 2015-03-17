08437ea01f4419cb62c5c887f9b57f873eb38795
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 16:34:00 -0700

    Create server.cs

---
+++ Add-Ons/RP_Drugs/server.cs
@@ -0,0 +1,17 @@
+// ============================================================
+// Project				:	CRPG
+// Author				:	Iban & Jookia
+// Description			:	Code used by all walks of life.
+// ============================================================
+// Table of Contents
+// 1. Idiot Test
+// 2. LAN Test
+// 3. File Execution
+// 4. Required Add-on loading
+// ============================================================
+//Core Files
+exec("./bricks.cs");
+exec("./common.cs");
+exec("./package.cs");
+exec("./prefs.cs");
+exec("./items/tools/knife.cs");
