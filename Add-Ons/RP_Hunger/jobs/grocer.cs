78ae33e9f59c82683ccc1be67ee3eb6bf1069896
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 16:45:39 -0700

    Create grocer.cs

---
+++ Add-Ons/RP_Hunger/jobs/grocer.cs
@@ -0,0 +1,33 @@
+// ============================================================
+// Project				:	CityRPG
+// Author				:	/Ty
+// Description			:	Grocer Code file
+// ============================================================
+
+$CityRPG::jobs::name = "Grocer";
+$CityRPG::jobs::type = "shop";
+$CityRPG::jobs::initialInvestment = 50;
+$CityRPG::jobs::pay = 25;
+$CityRPG::jobs::tools = "";
+$CityRPG::jobs::datablock = Player9SlotPlayer;
+$CityRPG::jobs::education = 1;
+
+$CityRPG::jobs::sellItems = false;
+$CityRPG::jobs::sellFood = true;
+$CityRPG::jobs::sellServices = false;
+
+$CityRPG::jobs::law = false;
+$CityRPG::jobs::canPardon = false;
+
+$CityRPG::jobs::thief = false;
+$CityRPG::jobs::hideJobName = false;
+
+$CityRPG::jobs::offerer = false;
+$CityRPG::jobs::claimer = false;
+
+$CityRPG::jobs::labor = false;
+
+$CityRPG::jobs::tmHexColor = "00FF00";
+$CityRPG::jobs::helpline = "\c3Grocers can sell food to other people.";
+
+$CityRPG::jobs::outfit = "none none none none greenShirt blackShirt skin blackPants blackShoes default worm-sweater";
