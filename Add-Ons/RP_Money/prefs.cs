d0aa3921088276c1fef27cb92156dad698fb20e4
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 17:06:43 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Money/prefs.cs
@@ -0,0 +1,212 @@
+// --------------------------
+// RP Money :: Standard preferences
+// --------------------------
+
+// Server
+$CRPG::pref::moneyDieTime = 60000;
+$CRPG::prices::vehicleSpawn = 2500;
+$RP::pref::server::brickCost = 0;
+
+// User
+
+$RP::pref::user::startMoney = 100;
+$RP::pref::user::tickMoney = 10;
+
+// Misc
+
+$RP::pref::moneyName = "Money";
+$RP::pref::moneyPrefix = "$";
+$RP::pref::moneySuffix = "";
+$RP::pref::loan::maximum = 2000;
+$RP::pref::loan::payback = 20;
+// These values are in per mil (tenth percent)
+$RP::pref::interest::bank = 1;
+$RP::pref::interest::loan = 10;
+// Item cost
+$RP::pref::item::cost = 100;
+$RP::pref::item::multiplier = 1;
+
+$CRPG::prices::weapon::name[1] = "gc_AssaultRifleItem";
+$CRPG::prices::weapon::uiName[1] = "Assault_Rifle";
+$CRPG::prices::weapon::price[1] = 400;
+$CRPG::prices::weapon::mineral[1] = 20;
+
+$CRPG::prices::weapon::name[2] = "gc_AssaultRifleSDItem";
+$CRPG::prices::weapon::uiName[2] = "Assault_Rifle_SD";
+$CRPG::prices::weapon::price[2] = 525;
+$CRPG::prices::weapon::mineral[2] = 25;
+
+$CRPG::prices::weapon::name[3] = "rpgAxeItem";
+$CRPG::prices::weapon::uiName[3] = "Axe";
+$CRPG::prices::weapon::price[3] = 200;
+$CRPG::prices::weapon::mineral[3] = 10;
+
+$CRPG::prices::weapon::name[4] = "gc_BattleRifleItem";
+$CRPG::prices::weapon::uiName[4] = "Battle_Rifle";
+$CRPG::prices::weapon::price[4] = 500;
+$CRPG::prices::weapon::mineral[4] = 30;
+
+$CRPG::prices::weapon::name[5] = "gc_BattleRifleSDItem";
+$CRPG::prices::weapon::uiName[5] = "Battle_Rifle_SD";
+$CRPG::prices::weapon::price[5] = 625;
+$CRPG::prices::weapon::mineral[5] = 35;
+
+$CRPG::prices::weapon::name[6] = "gc_BullpupItem";
+$CRPG::prices::weapon::uiName[6] = "Bullpup";
+$CRPG::prices::weapon::price[6] = 575;
+$CRPG::prices::weapon::mineral[6] = 5;
+
+$CRPG::prices::weapon::name[7] = "gc_FireaxeItem";
+$CRPG::prices::weapon::uiName[7] = "Fireaxe";
+$CRPG::prices::weapon::price[7] = 70;
+$CRPG::prices::weapon::mineral[7] = 15;
+
+$CRPG::prices::weapon::name[8] = "fistItem";
+$CRPG::prices::weapon::uiName[8] = "Fist";
+$CRPG::prices::weapon::price[8] = 0;
+$CRPG::prices::weapon::mineral[8] = 0;
+
+$CRPG::prices::weapon::name[9] = "gc_FlamethrowerItem";
+$CRPG::prices::weapon::uiName[9] = "Flamethrower";
+$CRPG::prices::weapon::price[9] = 240;
+$CRPG::prices::weapon::mineral[9] = 20;
+
+$CRPG::prices::weapon::name[10] = "gc_gasCanItem";
+$CRPG::prices::weapon::uiName[10] = "Gas_Canister";
+$CRPG::prices::weapon::price[10] = 40;
+$CRPG::prices::weapon::mineral[10] = 40;
+
+$CRPG::prices::weapon::name[11] = "GrappleRope";
+$CRPG::prices::weapon::uiName[11] = "Grapple_Rope";
+$CRPG::prices::weapon::price[11] = 235;
+$CRPG::prices::weapon::mineral[11] = 20;
+
+$CRPG::prices::weapon::name[12] = "gc_GrenadeLauncherItem";
+$CRPG::prices::weapon::uiName[12] = "Grenade_Launcher";
+$CRPG::prices::weapon::price[12] = 685;
+$CRPG::prices::weapon::mineral[12] = 40;
+
+$CRPG::prices::weapon::name[13] = "gc_HandcannonItem";
+$CRPG::prices::weapon::uiName[13] = "Handcannon";
+$CRPG::prices::weapon::price[13] = 300;
+$CRPG::prices::weapon::mineral[13] = 15;
+
+$CRPG::prices::weapon::name[14] = "gc_KalashnikovItem";
+$CRPG::prices::weapon::uiName[14] = "Kalashnikov";
+$CRPG::prices::weapon::price[14] = 155;
+$CRPG::prices::weapon::mineral[14] = 15;
+
+$CRPG::prices::weapon::name[15] = "keyItem";
+$CRPG::prices::weapon::uiName[15] = "Key";
+$CRPG::prices::weapon::price[15] = 2;
+$CRPG::prices::weapon::mineral[15] = 5;
+
+$CRPG::prices::weapon::name[16] = "gc_MGItem";
+$CRPG::prices::weapon::uiName[16] = "Machinegun";
+$CRPG::prices::weapon::price[16] = 3400;
+$CRPG::prices::weapon::mineral[16] = 35;
+
+$CRPG::prices::weapon::name[17] = "ZombieMedpackItem";
+$CRPG::prices::weapon::uiName[17] = "Medpack";
+$CRPG::prices::weapon::price[17] = 135;
+$CRPG::prices::weapon::mineral[17] = 5;
+
+$CRPG::prices::weapon::name[18] = "ParaItem";
+$CRPG::prices::weapon::uiName[18] = "Parachute";
+$CRPG::prices::weapon::price[18] = 180;
+$CRPG::prices::weapon::mineral[18] = 10;
+
+$CRPG::prices::weapon::name[19] = "rpgPickaxeItem";
+$CRPG::prices::weapon::uiName[19] = "Pickaxe";
+$CRPG::prices::weapon::price[19] = 125;
+$CRPG::prices::weapon::mineral[19] = 10;
+
+$CRPG::prices::weapon::name[20] = "pipeWrenchItem";
+$CRPG::prices::weapon::uiName[20] = "Pipe_Wrench";
+$CRPG::prices::weapon::price[20] = 10;
+$CRPG::prices::weapon::mineral[20] = 10;
+
+$CRPG::prices::weapon::name[21] = "gc_PistolItem";
+$CRPG::prices::weapon::uiName[21] = "Pistol";
+$CRPG::prices::weapon::price[21] = 325;
+$CRPG::prices::weapon::mineral[21] = 15;
+
+$CRPG::prices::weapon::name[22] = "gc_PistolSDItem";
+$CRPG::prices::weapon::uiName[22] = "Pistol_SD";
+$CRPG::prices::weapon::price[22] = 375;
+$CRPG::prices::weapon::mineral[22] = 20;
+
+$CRPG::prices::weapon::name[23] = "gc_RevolverItem";
+$CRPG::prices::weapon::uiName[23] = "Revolver";
+$CRPG::prices::weapon::price[23] = 300;
+$CRPG::prices::weapon::mineral[23] = 20;
+
+$CRPG::prices::weapon::name[24] = "RiotShieldItem";
+$CRPG::prices::weapon::uiName[24] = "Riot_Shield";
+$CRPG::prices::weapon::price[24] = 300;
+$CRPG::prices::weapon::mineral[24] = 20;
+
+$CRPG::prices::weapon::name[25] = "RodItem";
+$CRPG::prices::weapon::uiName[25] = "Rod";
+$CRPG::prices::weapon::price[25] = 45;
+$CRPG::prices::weapon::mineral[25] = 10;
+
+$CRPG::prices::weapon::name[26] = "gc_ShotgunItem";
+$CRPG::prices::weapon::uiName[26] = "Shotgun";
+$CRPG::prices::weapon::price[26] = 335;
+$CRPG::prices::weapon::mineral[26] = 20;
+
+$CRPG::prices::weapon::name[27] = "SkiItem";
+$CRPG::prices::weapon::uiName[27] = "Skis";
+$CRPG::prices::weapon::price[27] = 55;
+$CRPG::prices::weapon::mineral[27] = 5;
+
+$CRPG::prices::weapon::name[28] = "gc_SMGItem";
+$CRPG::prices::weapon::uiName[28] = "SMG";
+$CRPG::prices::weapon::price[28] = 250;
+$CRPG::prices::weapon::mineral[28] = 20;
+
+$CRPG::prices::weapon::name[29] = "gc_SMGSDItem";
+$CRPG::prices::weapon::uiName[29] = "SMG_SD";
+$CRPG::prices::weapon::price[29] = 350;
+$CRPG::prices::weapon::mineral[29] = 25;
+
+$CRPG::prices::weapon::name[30] = "gc_SniperRifleItem";
+$CRPG::prices::weapon::uiName[30] = "Sniper_Rifle";
+$CRPG::prices::weapon::price[30] = 335;
+$CRPG::prices::weapon::mineral[30] = 20;
+
+$CRPG::prices::weapon::name[31] = "gc_UziItem";
+$CRPG::prices::weapon::uiName[31] = "Uzi";
+$CRPG::prices::weapon::price[31] = 535;
+$CRPG::prices::weapon::mineral[31] = 10;
+
+$CRPG::prices::weapon::name[32] = "gc_UziSDItem";
+$CRPG::prices::weapon::uiName[32] = "Uzi_SD";
+$CRPG::prices::weapon::price[32] = 650;
+$CRPG::prices::weapon::mineral[32] = 15;
+
+$CRPG::prices::weapon::name[33] = "gc_TacticalVestItem";
+$CRPG::prices::weapon::uiName[33] = "Tactical_Vest";
+$CRPG::prices::weapon::price[33] = 50;
+$CRPG::prices::weapon::mineral[33] = 10;
+
+$CRPG::prices::weapon::name[34] = "C4Item";
+$CRPG::prices::weapon::uiName[34] = "C4_Explosive";
+$CRPG::prices::weapon::price[34] = 25;
+$CRPG::prices::weapon::mineral[34] = 15;
+
+$CRPG::prices::weapon::name[35] = "gc_TacticalHelmetItem";
+$CRPG::prices::weapon::uiName[35] = "Tactical_Helmet";
+$CRPG::prices::weapon::price[35] = 28;
+$CRPG::prices::weapon::mineral[35] = 10;
+
+$CRPG::prices::weapon::name[36] = "knifeItem";
+$CRPG::prices::weapon::uiName[36] = "Knife";
+$CRPG::prices::weapon::price[36] = 15;
+$CRPG::prices::weapon::mineral[36] = 2;
+
+$ListAmt = 36; // has to equal to the amount of items on the list
+//There can be no spaces in the uiName, otherwise the item mod will be broken
+
+$CRPG::pref::realestate::maxLots = 5;
