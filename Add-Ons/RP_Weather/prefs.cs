97d815edc5b28be37478fe3c567d92b5d7ad3123
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 17:28:48 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Weather/prefs.cs
@@ -0,0 +1,45 @@
+// -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// RP Weather :: Weather Chances & Current Day/Weather Prefs
+// -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// Weather Chances
+// -=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// Chance Of Rain
+$RP::pref::RainMonth1 = 5; // January
+$RP::pref::RainMonth2 = 10; // February
+$RP::pref::RainMonth3 = 15; // March
+$RP::pref::RainMonth4 = 20; // April
+$RP::pref::RainMonth5 = 30; // May
+$RP::pref::RainMonth6 = 40; // June
+$RP::pref::RainMonth7 = 50; // July
+$RP::pref::RainMonth8 = 40; // August
+$RP::pref::RainMonth9 = 30; // September
+$RP::pref::RainMonth10 = 20; // October
+$RP::pref::RainMonth11 = 15; // November
+$RP::pref::RainMonth12 = 0; // December
+	
+// Chance Of Snow
+$RP::pref::SnowMonth1 = 30; // January
+$RP::pref::SnowMonth2 = 15; // February
+$RP::pref::SnowMonth3 = 5; // March
+$RP::pref::SnowMonth4 = 0; // April
+$RP::pref::SnowMonth5 = 0; // May
+$RP::pref::SnowMonth6 = 0; // June
+$RP::pref::SnowMonth7 = 0; // July
+$RP::pref::SnowMonth8 = 0; // August
+$RP::pref::SnowMonth9 = 0; // September
+$RP::pref::SnowMonth10 = 5; // October
+$RP::pref::SnowMonth11 = 15; // November
+$RP::pref::SnowMonth12 = 100; // December
+
+// Misc
+$RP::pref::CurrWeather = "Clear";
+$RP::pref::radioname = "News Radio";
+$RP::pref::radiofreq = "103.5";
+$RP::pref::FogIntensity = "0";
+$RP::pref::altersky = 1;
+$RP::pref::altersun = 1;
+$RP::pref::altersunfx = 1;
+$RP::pref::Weather = 1;
+$MapAltered = 0;
