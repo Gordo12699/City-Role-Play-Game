c91b1609f65c9c577247cefb463107ad05d54128
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 17:23:50 -0700

    Create prefs.cs

---
+++ Add-Ons/RP_Resources/prefs.cs
@@ -0,0 +1,25 @@
+// --------------------------
+// RP Resources :: Standard preferences
+// --------------------------
+
+// Server
+
+$RP::pref::server::hardcoreExp = 500;
+
+// Gain
+
+$RP::pref::gain::exp::cutTree = 1;
+$RP::pref::gain::exp::mineOre = 1;
+$RP::pref::gain::exp::fishing = 1;
+$RP::pref::gain::money::wood = 1;
+$RP::pref::gain::money::ore = 1;
+$RP::pref::gain::money::fish = 1;
+
+$CRPG::citydata::minerals = 0;
+$CRPG::citydata::lumber = 0;
+
+// Misc
+
+$RP::pref::oreName = "Ore";
+$RP::pref::woodName = "Wood";
+$RP::pref::fishName = "Fish";
