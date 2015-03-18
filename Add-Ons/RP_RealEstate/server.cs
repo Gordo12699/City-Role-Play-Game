03d4540b09adab5eb2b3864045a09ad47357f920
Gordo12699 <Gordo12699@gmail.com>
2015-03-17 17:15:59 -0700

    Create server.cs

---
+++ Add-Ons/RP_RealEstate/server.cs
@@ -0,0 +1,13 @@
+// ============================================================
+// Section 3 : File Execution
+// ============================================================
+
+//Core Files
+exec("./bricks.cs");
+exec("./commands.cs");
+exec("./events.cs");
+exec("./game.cs");
+exec("./help.cs");
+exec("./prefs.cs");
+exec("./saving.cs");
+exec("./triggers.cs");
